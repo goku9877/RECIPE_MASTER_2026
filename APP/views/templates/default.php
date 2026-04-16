7fd5c3eaf8bbb4e20fc45caf2a09fc386b4c20d1
othmane <gogeta19981090@gmail.com>
2026-04-16 13:20:55 +0200

    version pascal

---
+++ APP/views/templates/default.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include '../app/views/templates/partials/_head.php'; ?>
+</head>
+
+<body
+    class="bg-gray-100 text-gray-800 font-sans leading-normal tracking-normal">
+
+    <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+    <?php include '../app/views/templates/partials/_main.php'; ?>
+
+    <?php include '../app/views/templates/partials/_footer.php'; ?>
+
+</body>
+
+</html>
